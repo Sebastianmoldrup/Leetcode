ef399bb95dab5b096629b082f81e322540be0ef0
Sebastianmoldrup <Sebastian.moldrup@gmail.com>
2023-04-21 02:16:19 +0200

    updated task 1768

--- Task_1768.js
+++ Task_1768.js
@@ -1,20 +1,24 @@
-const word1 = 'abc';
-const word2 = 'pqrds';
+const word1 = 'fb';
+const word2 = 'ce';
 
-// const c =
-//   [...word1].reduce((acc, cur, i) => acc + cur + word2[i], '') +
-//   word2.slice(word1.length);
-
-// [...word1] = ['a', 'b', 'c']
-// .reduce((acc, cur, i) => acc + cur + word2[i], '')
-
-const [longest, shortest] =
+/* const [longest, shortest] =
   word1 > word2
     ? [word1.split(''), word2.split('')]
     : [word2.split(''), word1.split('')];
 
 const red =
-  [...shortest].reduce((prev, cur) => prev + cur + longest.shift(), '') +
-  longest.join('');
+  [...shortest].reduce((prev, cur) => {
+    const alt = longest.length ? longest.shift() : '';
+    return prev + cur + alt;
+  }, '') + longest.join('');
+
+console.log(red); */
+
+word2 = [...word2];
+const red =
+  [...word1].reduce((combined, char) => {
+    const alt = word2.length ? word2.shift() : '';
+    return combined + char + alt;
+  }, '') + longest.join('');
 
 console.log(red);
